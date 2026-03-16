a92ee980acf98b1ebaafa96bcffc82738decaab5
Jonhatan <jonhatantale@gmail.com>
2026-03-16 00:26:11 -0600

    Inicio del menu

--- ProyectoStreaming/ProyectoStreaming/Program.cs
+++ ProyectoStreaming/ProyectoStreaming/Program.cs
@@ -1,3 +1,40 @@
-﻿Console.WriteLine("Hola bbs JAJAJAJ");
+﻿Console.WriteLine("Menú de opciones");
+Console.WriteLine("1. Evaluar contenido");
+Console.WriteLine("2. Mostrar reglas del sistema");
+Console.WriteLine("3. Mostrar estadisticas  de la sesión");
+Console.WriteLine("4. Reiniciar estadísticas");
+Console.WriteLine("5. Salir");
+int menu = int.Parse(Console.ReadLine());
 
-Console.WriteLine("Prueba dos xdd");
+do
+{
+
+    switch (menu)
+    {
+        case 1:
+            Console.WriteLine("Ingrese tipo de contenido(película, serie ,documental, evento en vivo)");
+            string tcontenido = Console.ReadLine();
+
+            Console.WriteLine("Ingrese la duración en minutos");
+            int duracion = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la clasificación(todo publico, +13, +18)";
+            string clasificacion = Console.ReadLine();
+
+            Console.WriteLine("Ingrese la hora programada para transmitir(0-23)");
+            int transmision = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese nivel de producción(bajo, medio, alto)");
+            string produccion = Console.ReadLine();
+
+            /*if ()
+            {
+
+            }
+            */
+
+            break;
+
+    }
+
+} while (menu != 0);
