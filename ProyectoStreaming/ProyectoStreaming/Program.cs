ec576ab8dc881e52513fe33b0a0ce3c040c65d26
Jonhatan <jonhatantale@gmail.com>
2026-03-16 10:56:58 -0600

    Prueba

--- ProyectoStreaming/ProyectoStreaming/Program.cs
+++ ProyectoStreaming/ProyectoStreaming/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine("Menú de opciones");
+﻿using System.Numerics;
+
+Console.WriteLine("Menú de opciones");
 Console.WriteLine("1. Evaluar contenido");
 Console.WriteLine("2. Mostrar reglas del sistema");
 Console.WriteLine("3. Mostrar estadisticas  de la sesión");
@@ -12,24 +14,43 @@ do
     switch (menu)
     {
         case 1:
+            string[] contValidos = { "pelicula", "serie", "documental", "evento en vivo" };
             Console.WriteLine("Ingrese tipo de contenido(película, serie ,documental, evento en vivo)");
-            string tcontenido = Console.ReadLine();
+            string contenidos = Console.ReadLine();
 
-            Console.WriteLine("Ingrese la duración en minutos");
-            int duracion = int.Parse(Console.ReadLine());
+            if (Array.Exists(contValidos, cont => cont == contenidos))
+            {
+               
+                Console.WriteLine("Ingrese la duración en minutos");
+                int duracion = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Ingrese la clasificación(todo publico, +13, +18)");
-            string clasificacion = Console.ReadLine();
+                if (duracion >= 20 && duracion <= 240)
+                {
+                    string[] clasValidas = {"todo publico", "+13", "+18" };
+                    Console.WriteLine("Ingrese la clasificación(todo publico, +13, +18)");
+                    string clasificacion = Console.ReadLine();
 
-            Console.WriteLine("Ingrese la hora programada para transmitir(0-23)");
-            int transmision = int.Parse(Console.ReadLine());
+                    if (Array.Exists(clasValidas, clas => clas == clasificacion))
+                    {
 
-            Console.WriteLine("Ingrese nivel de producción(bajo, medio, alto)");
-            string produccion = Console.ReadLine();
-            
+                        Console.WriteLine("Ingrese la hora programada para transmitir(0-23)");
+                        int transmision = int.Parse(Console.ReadLine());
+                        if ()
+                        {
+                            Console.WriteLine("Ingrese nivel de producción(bajo, medio, alto)");
+                            string produccion = Console.ReadLine();
 
-            break;
+                        }
+                    }
+
+                }
+            }
+
+        break;
 
     }
 
 } while (menu != 5);
+
+
+Console.WriteLine("holas ");
